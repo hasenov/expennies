ce1d7b7f126737a73d00fca4655e0e986679cdbe
Aidar <aidarwebdev@gmail.com>
2025-02-26 00:30:28 +0500

    project setup

---
+++ configs/container/container_bindings.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types = 1);
+
+use App\Config;
+use App\Enum\AppEnvironment;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+use Psr\Container\ContainerInterface;
+use Slim\Views\Twig;
+use Symfony\Bridge\Twig\Extension\AssetExtension;
+use Symfony\Component\Asset\Package;
+use Symfony\Component\Asset\Packages;
+use Symfony\Component\Asset\VersionStrategy\JsonManifestVersionStrategy;
+use Symfony\WebpackEncoreBundle\Asset\EntrypointLookup;
+use Symfony\WebpackEncoreBundle\Asset\EntrypointLookupCollection;
+use Symfony\WebpackEncoreBundle\Asset\TagRenderer;
+use Symfony\WebpackEncoreBundle\Twig\EntryFilesTwigExtension;
+use Twig\Extra\Intl\IntlExtension;
+
+use function DI\create;
+
+return [
+    Config::class                 => create(Config::class)->constructor(require CONFIG_PATH . '/app.php'),
+    EntityManager::class          => fn(Config $config) => new EntityManager(
+        $config->get('doctrine.connection'),
+        ORMSetup::createAttributeMetadataConfiguration(
+            $config->get('doctrine.entity_dir'),
+            $config->get('doctrine.dev_mode')
+        )
+    ),
+    Twig::class                   => function (Config $config, ContainerInterface $container) {
+        $twig = Twig::create(VIEW_PATH, [
+            'cache'       => STORAGE_PATH . '/cache/templates',
+            'auto_reload' => AppEnvironment::isDevelopment($config->get('app_environment')),
+        ]);
+
+        $twig->addExtension(new IntlExtension());
+        $twig->addExtension(new EntryFilesTwigExtension($container));
+        $twig->addExtension(new AssetExtension($container->get('webpack_encore.packages')));
+
+        return $twig;
+    },
+
+    /**
+     * The following two bindings are needed for EntryFilesTwigExtension & AssetExtension to work for Twig
+     */
+    'webpack_encore.packages'     => fn() => new Packages(
+        new Package(new JsonManifestVersionStrategy(BUILD_PATH . '/manifest.json'))
+    ),
+
+    'webpack_encore.tag_renderer' => fn(ContainerInterface $container) => new TagRenderer(
+        $container->get(EntrypointLookupCollection::class),
+        $container->get('webpack_encore.packages')
+    ),
+
+    '_default' => fn() => new EntrypointLookup(BUILD_PATH . '/entrypoints.json'),
+];
